3444c9d214fbb5c45eb5a3361f714f3b1721faf0
RovicAlmonia <rovicmatthew0311@gmail.com>
2025-04-07 23:16:04 +0800

    what

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from . import views
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TodoViewSet
+
+router = DefaultRouter()
+router.register(r'todos', TodoViewSet, basename='todo')
 
 urlpatterns = [
-    path('todos/', views.todo_list, name='todo_list'),
-    path('todos/<int:pk>/', views.todo_detail, name='todo_detail'),
+    path('', include(router.urls)),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,43 +1,52 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Todo
 from .serializers import TodoSerializer
+from django.http import Http404
+from rest_framework import viewsets
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
 # List all todos or create a new one
-@api_view(['GET', 'POST'])
-def todo_list(request):
-    if request.method == 'GET':
+class TodoList(APIView):
+    def get(self, request, format=None):
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Retrieve, update, or delete a single todo by ID
-@api_view(['GET', 'PATCH', 'DELETE'])
-def todo_detail(request, pk):
-    try:
-        todo = Todo.objects.get(pk=pk)
-    except Todo.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# Retrieve, update, or delete a specific todo
+class TodoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        todo = self.get_object(pk)
         serializer = TodoSerializer(todo)
         return Response(serializer.data)
 
-    elif request.method == 'PATCH':
+    def patch(self, request, pk, format=None):  # Use PATCH for partial updates
+        todo = self.get_object(pk)
         serializer = TodoSerializer(todo, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        todo = self.get_object(pk)
         todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
